6e6fd6141b779a7e192310d6aad008c1b9e7cf1b
Alex <95600375+top-droid@users.noreply.github.com>
2022-05-21 23:06:25 +0200

    finished website

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('hello')
+
+const navToggle = document.querySelector ('.nav-toggle');
+const navLinks = document.querySelectorAll('nav__links')
+
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+
+navLinks.forEach(link => {
+    link.addEventListener ('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+
